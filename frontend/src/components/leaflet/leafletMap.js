81a2df246d9aed9abf3f7ed816c22848483fd2fd
OJMatGH <omarjoemaalouly@hotmail.com>
2023-11-15 11:23:29 +0200

    map fixed

--- frontend/src/components/leaflet/leafletMap.js
+++ frontend/src/components/leaflet/leafletMap.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './leafletMap.css'
 
@@ -14,12 +14,11 @@ const leafletMap = () => {
         <MapContainer center={position} zoom={8} style={{ width: '100%', height: '400px' }}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
+                />
             <Marker position={position}>
-                <Popup>
+                {/* <Popup>
                     Beirut, Lebanon
-                </Popup>
+                </Popup> */}
             </Marker>
         </MapContainer>
         </div>
