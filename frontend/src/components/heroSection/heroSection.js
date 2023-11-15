d0b4d95031abdaeba8cfb241fbdcfe5f14a577ad
OJMatGH <omarjoemaalouly@hotmail.com>
2023-11-15 10:24:09 +0200

    added hero section

---
+++ frontend/src/components/heroSection/heroSection.js
@@ -0,0 +1,13 @@
+import  '../heroSection/heroSection.css'
+
+function HeroSection (){
+  return(
+    <section className="heroSection">
+        <h1><span>SOLAR</span>WAVE</h1>
+        <div className='seperator'></div>
+        <h2>Your Energy <span>Solutions</span></h2>
+        
+    </section>
+  )
+};
+export default HeroSection;
---
+++ frontend/src/layouts/home.js
@@ -0,0 +1,11 @@
+import HeroSection from '../components/heroSection/heroSection';
+
+function Home() {
+  return (
+    <div className="Home">
+        <HeroSection/>
+    </div>
+  );
+}
+
+export default Home;
